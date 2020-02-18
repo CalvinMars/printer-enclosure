9fa077c8a8df3e763878de17dbf2c8b3b3387c74
jrmuys <jrmuyskens@gmail.com>
2020-02-18 10:18:23 -0500

    Update enclosure_program.py

--- enclosure_program.py
+++ enclosure_program.py
@@ -1,18 +1,21 @@
 import time
 import RPi.GPIO as GPIO
-from neopixel import *
-import argparse
+# from neopixel import *
+# import argparse
 
-# LED strip configuration:
-LED_COUNT      = 68      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# # LED strip configuration:
+# LED_COUNT      = 68      # Number of LED pixels.
+# LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+# #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+# LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+# LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+# LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+# LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+# LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+import board
+import neopixel
+pixels = neopixel.NeoPixel(board.D18, 68)
 
 # Find temperature probe
 DS18B20="/sys/bus/w1/devices/28-000009a29c16/w1_slave"
@@ -33,20 +36,23 @@ button_state = 0
 
 
 # Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
+# def colorWipe(strip, color, wait_ms=50):
+#     """Wipe color across display a pixel at a time."""
+#     for i in range(strip.numPixels()):
+#         strip.setPixelColor(i, color)
+#         strip.show()
+#         time.sleep(wait_ms/1000.0)
 
 
 if __name__ == '__main__':
     # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
-    strip.begin()
-    colorWipe(strip, Color(255,255,255))
+    # strip.begin()
+    # colorWipe(strip, Color(255,255,255))
+    for x in range(0, 68):
+        pixels[x] = (255, 255, 255)
+        sleep(1)
     while True:
         # Get reading from probe
         f = open(DS18B20, "r")
